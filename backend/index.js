911700654e74515eeb12b00c37c43380c9ac7bc9
ilya_yakovenko <ilia10d2020@gmail.com>
2026-02-24 01:31:13 +0400

    chore: инициализация проекта, структура, зависимости, базовый сервер, README и .gitignore

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Тестовый маршрут
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello from Events API!',
+    status: 'ok',
+    time: new Date().toISOString()
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
+});
